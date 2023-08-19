0dfa681c02a849287abed7a8200fce503da6dca9
Yevhenii Malinovskiy <118572078+Travellin91@users.noreply.github.com>
2023-08-19 14:46:45 +0300

    I have developed a full-fledged REST API for managing contacts using Node.js, Express, and MongoDB. Your API allows you to:
    I have developed a full-fledged REST API for managing contacts using Node.js, Express, and MongoDB. Your API allows you to:
    
    Retrieve contact data.
    Create new contacts.
    Update contact information and their "favorite" flag.
    Delete contacts.
    I set up a connection to the MongoDB Atlas database, utilizing confidential data from the .env file. Your API runs on a server, listens for incoming requests, and provides responses in JSON format. The code is organized into models, controllers, and routes for better structuring.

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,25 +1,70 @@
-const express = require('express')
+const express = require("express");
+const router = express.Router();
+const contactsController = require("../../controllers/contacts");
 
-const router = express.Router()
+router.get("/contacts", async (req, res) => {
+  try {
+    const contacts = await contactsController.getAll();
+    res.json(contacts);
+  } catch (error) {
+    res.status(500).json({ message: "Error fetching contacts" });
+  }
+});
 
-router.get('/', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+router.get("/contacts/:id", async (req, res) => {
+  const contactId = req.params.id;
+  try {
+    const contact = await contactsController.getOne(contactId);
+    if (!contact) {
+      res.status(404).json({ message: "Contact not found" });
+      return;
+    }
+    res.json(contact);
+  } catch (error) {
+    res.status(500).json({ message: "Error fetching contact" });
+  }
+});
 
-router.get('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+router.post("/contacts", async (req, res) => {
+  const newContactData = req.body;
+  try {
+    const newContact = await contactsController.create(newContactData);
+    res.status(201).json(newContact);
+  } catch (error) {
+    res.status(500).json({ message: "Error creating contact" });
+  }
+});
 
-router.post('/', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+router.put("/contacts/:id", async (req, res) => {
+  const contactId = req.params.id;
+  const updatedContactData = req.body;
+  try {
+    const updatedContact = await contactsController.update(contactId, updatedContactData);
+    res.json(updatedContact);
+  } catch (error) {
+    res.status(500).json({ message: "Error updating contact" });
+  }
+});
 
-router.delete('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+router.patch("/contacts/:id/favorite", async (req, res) => {
+  const contactId = req.params.id;
+  const newFavoriteValue = req.body.favorite;
+  try {
+    const updatedContact = await contactsController.updateFavorite(contactId, newFavoriteValue);
+    res.json(updatedContact);
+  } catch (error) {
+    res.status(500).json({ message: "Error updating contact's favorite status" });
+  }
+});
 
-router.put('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+router.delete("/contacts/:id", async (req, res) => {
+  const contactId = req.params.id;
+  try {
+    const deletedContact = await contactsController.remove(contactId);
+    res.json(deletedContact);
+  } catch (error) {
+    res.status(500).json({ message: "Error deleting contact" });
+  }
+});
 
-module.exports = router
+module.exports = router;
